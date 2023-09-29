9a008d0a472760f632e1f905b0fa451304c3bfa1
Stephan <vonzeo7@gmail.com>
2023-09-28 22:19:36 -0300

    more week 16

--- Exercises/Week16/TicTacToe-1/colorGame00.js
+++ Exercises/Week16/TicTacToe-1/colorGame00.js
@@ -1,15 +1,45 @@
+const Square = ({ id, player }) => {
+  const [color, setColor] = React.useState("green");
+  const palet = ["red", "blue", "green"];
+  const getRandomColor = () => palet[Math.floor(Math.random() * 3)];
+
+  return (
+    <button
+      onClick={(e) => {
+        setColor(getRandomColor());
+        e.target.style.background = color;
+      }}
+    >
+      <h1>{player}</h1>
+    </button>
+  );
+};
+
 const Board = () => {
-  const [player, setPlayer] = React.useState(0);
+  const [player, setPlayer] = React.useState(1);
+  const [mounted, setMounted] = React.useState(true);
   let status = `Player ${player}`;
+  const toggle = () => setMounted(!mounted);
+
+
+  function renderSquare(i) {
+    return <Square id={i} player={player}></Square>;
+  }
   return (
     <div
       className="game-board"
       onClick={(e) => {
-        setPlayer(player + 1);
+        setPlayer(player === 1 ? 2 : 1);
         status = `Player ${player}`;
       }}
     >
+      <div className="grid-row">
+        {mounted && renderSquare(0)}
+        {mounted && renderSquare(1)}
+        {mounted && renderSquare(2)}
+      </div>
       <div id="info">
+        <button onClick={toggle}>Show/Hide Row</button>
         <h1>{status}</h1>
       </div>
     </div>
